b2a164198ef7f571a29310e771f136e4b211c975
Mateusz Walczyk <mattiusw@gmail.com>
2023-09-16 13:39:37 +0200

    Hello World!

---
+++ kursSpring/src/main/java/com/example/kursSpring/Hi.java
@@ -0,0 +1,14 @@
+package com.example.kursSpring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Hi implements CommandLineRunner
+{
+    @Override
+    public void run(String... args) throws Exception
+    {
+        System.out.println("Hello World!!!");
+    }
+}
