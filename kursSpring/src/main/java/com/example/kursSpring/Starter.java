e60dee4e129adcfc3350dee9780c7b54d2aee012
Mateusz Walczyk <mattiusw@gmail.com>
2023-09-17 10:12:47 +0200

    Use better kit to get bean

--- kursSpring/src/main/java/com/example/kursSpring/Starter.java
+++ kursSpring/src/main/java/com/example/kursSpring/Starter.java
@@ -1,13 +1,17 @@
 package com.example.kursSpring;
 
+import com.example.kursSpring.domain.Castle;
 import com.example.kursSpring.domain.Knight;
 import com.example.kursSpring.domain.Quest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Starter implements CommandLineRunner
 {
+    @Autowired
+    Castle castle;
     @Override
     public void run(String... args) throws Exception
     {
