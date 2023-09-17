f651c9e5e3ccefda921f301f55923bd328dc690e
Mateusz Walczyk <mattiusw@gmail.com>
2023-09-17 10:08:06 +0200

    Wrong use get bean

--- kursSpring/src/main/java/com/example/kursSpring/KursSpringApplication.java
+++ kursSpring/src/main/java/com/example/kursSpring/KursSpringApplication.java
@@ -11,8 +11,12 @@ public class KursSpringApplication
 
 	public static void main(String[] args)
 	{
+		SpringApplication.run(KursSpringApplication.class, args);
+		/* Zly zapis, sa lepsze narzedzia
 		ConfigurableApplicationContext ctx = SpringApplication.run(KursSpringApplication.class, args);
 		Castle castle = (Castle)ctx.getBean("castle");
+		System.out.println(castle);
+		*/
 	}
 
 }
