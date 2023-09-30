d23d1333e8dd983253f4370e814d22bfc0de4023
Mateusz Walczyk <mattiusw@gmail.com>
2023-09-30 19:33:52 +0200

    Make field and constructor

--- kursspring/src/main/java/com/clockworkjava/kursspring/domain/Knight.java
+++ kursspring/src/main/java/com/clockworkjava/kursspring/domain/Knight.java
@@ -1,4 +1,16 @@
 package com.clockworkjava.kursspring.domain;
 
 public class Knight {
+    private String name;
+    private int age;
+
+    public Knight(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString(){
+        return "Rycerz o imieniu " + name + "( " + age + " )";
+    }
 }
