735cbd2e6e4a0a43fc779a20630bd2a11012b33d
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-06 18:13:09 +0200

    Make class Castle

--- kursspring/src/main/java/com/example/kursspring/Starter.java
+++ kursspring/src/main/java/com/example/kursspring/Starter.java
@@ -1,6 +1,7 @@
 package com.example.kursspring;
 
 import com.example.kursspring.domain.Knight;
+import com.example.kursspring.domain.Quest;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +10,14 @@ public class Starter implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception{
-        Knight lancelot = new Knight("Lancelot", 29);
+
+        Quest saveThePrincess = new Quest("Uratuj ksiezniczke");
+        Knight lancelot = new Knight("Lancelot", 29, saveThePrincess);
         System.out.println(lancelot);
+
+        Quest killTheDragon = new Quest("Zabij smoka");
+        Knight percival = new Knight("Percival", 32);
+        percival.setQuest(killTheDragon);
+        System.out.println(percival);
     }
 }
---
+++ kursspring/src/main/java/com/example/kursspring/domain/Castle.java
@@ -0,0 +1,13 @@
+package com.example.kursspring.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Castle {
+
+    private String name = "East Watch";
+
+    public Castle(){
+
+    }
+}
