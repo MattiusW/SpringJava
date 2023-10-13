c7a3a82e3feaef53a385562610ab4f2834da2d34
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-13 18:28:55 +0200

    @Qualifer

--- kursspring/src/main/java/com/example/kursspring/config/MainConfig.java
+++ kursspring/src/main/java/com/example/kursspring/config/MainConfig.java
@@ -3,36 +3,28 @@ package com.example.kursspring.config;
 import com.example.kursspring.domain.Castle;
 import com.example.kursspring.domain.Knight;
 import com.example.kursspring.domain.Quest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 
 @Configuration
 //@ImportResource("classpath:config/castle-config.xml")
 public class MainConfig {
 
+    @Autowired
+    Quest quest;
+    @Bean(name="lancelot")
+    @Primary
+    public Knight lancelot(){
+        Knight lancelot = new Knight("Lancelot", 29);
+        lancelot.setQuest(quest);
+        return lancelot;
+    }
 
-//    @Bean
-//    public Quest createQuest(){
-//        return new Quest();
-//    }
-
-//    @Bean
-//    @Scope("prototype")
-//    public Knight knight(){
-//        Knight knight = new Knight("Lancelot", 29);
-//        knight.setQuest(createQuest());
-//        return knight;
-//    }
-//
-//    @Bean(name="zamek", initMethod = "build", destroyMethod = "tearDown")
-//    @Value("${my.castle.name:East Watch}")
-//    public Castle castle(String name){
-//        Castle castle = new Castle(knight());
-//        castle.setName(name);
-//
-//        return castle;
-//    }
+    @Bean(name="percival")
+    public Knight pervcival(){
+        Knight percival = new Knight("Percival", 25);
+        percival.setQuest(quest);
+        return percival;
+    }
 }
--- kursspring/src/main/java/com/example/kursspring/domain/Castle.java
+++ kursspring/src/main/java/com/example/kursspring/domain/Castle.java
@@ -3,6 +3,7 @@ package com.example.kursspring.domain;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
