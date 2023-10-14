c41117eb207b7462e95820cd3502fbe43f778979
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-14 17:08:52 +0200

    Import Json reaload Maven

--- Books/src/main/java/org/example/domain/book/BookRepository.java
+++ Books/src/main/java/org/example/domain/book/BookRepository.java
@@ -1,7 +1,10 @@
 package org.example.domain.book;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
+import com.google.gson.Gson;
 
 public class BookRepository {
 
@@ -11,7 +14,19 @@ public class BookRepository {
         Random r = new Random();
         Book newOne = new Book(r.nextInt(), title);
         this.books.add(newOne);
-        System.out.println("Utworzylem nowa ksiazke o tytytule: " + newOne.getTitle());
+        System.out.println("Utworzylem nowa ksiazke o tytule: " + newOne.getTitle());
         return newOne;
+    }
+
+    void readAll(){
+
+    }
+
+    void saveAll() throws IOException {
+        Gson gson = new Gson(); //Reload Maven
+        FileWriter fw = new FileWriter("./books.json");
+        gson.toJson(this.books, fw);
+        fw.flush();
+        fw.close();
     }
 }
