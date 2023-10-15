26959941ab8bc13e3ccc146f2ed8916835265017
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-15 08:21:27 +0200

    Configuration xml

--- kursspring/src/main/java/com/example/kursspring/domain/Tournament.java
+++ kursspring/src/main/java/com/example/kursspring/domain/Tournament.java
@@ -3,9 +3,12 @@ package com.example.kursspring.domain;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.Map;
 
 @Component
 public class Tournament {
