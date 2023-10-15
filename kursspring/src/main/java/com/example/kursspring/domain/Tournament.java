cf6952d0b334b7759bef0f63e8c19623fe98ec0a
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-15 07:46:57 +0200

    Inject Set

--- kursspring/src/main/java/com/example/kursspring/domain/Tournament.java
+++ kursspring/src/main/java/com/example/kursspring/domain/Tournament.java
@@ -4,13 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
 public class Tournament {
 
     @Autowired
-    List<Knight> knights;
+    Set<Knight> knights;
 
     public Tournament(){
 
@@ -20,7 +21,7 @@ public class Tournament {
 //        knight.setAge(knight.getAge()+1);
     }
 
-    public void setKnight(List<Knight> knights){
+    public void setKnight(Set<Knight> knights){
         this.knights = knights;
     }
 
