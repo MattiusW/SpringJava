03153f05a62c4733f1ad825171d997e5a402d5ec
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-22 09:41:36 +0200

    Test quest

--- kursspring/src/test/java/com/example/kursspring/WebContextTest.java
+++ kursspring/src/test/java/com/example/kursspring/WebContextTest.java
@@ -1,9 +1,12 @@
 package com.example.kursspring;
 
 import com.example.kursspring.controllers.QuestController;
+import com.example.kursspring.domain.Knight;
 import com.example.kursspring.domain.PlayerInformation;
+import com.example.kursspring.domain.Quest;
 import com.example.kursspring.domain.services.KnightService;
 import com.example.kursspring.domain.services.QuestService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -13,6 +16,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
 
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
@@ -29,9 +40,30 @@ public class WebContextTest {
     @InjectMocks
     QuestController questController;
 
-    @Bean
-    public void setUp(){
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(questController).build();
+    }
+
+    public void testCheckQuest(){
+
+        Quest quest = new Quest("Zadanie test");
+
+        Knight percival = new Knight("Percival", 33);
+        Knight lancelot = new Knight("Lancelot", 36);
+
+        percival.setQuest(quest);
+
+        List<Knight> knights = new ArrayList<>(2);
+
+        knights.add(percival);
+        knights.add(lancelot);
+
+        when(knightService.getAllKnights()).thenReturn(knights);
+
     }
 
     @Test
