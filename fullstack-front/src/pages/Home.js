ee1c6c9abda21a2abf24088d5279559d56a5cde6
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-28 09:13:08 +0200

    Configure home user

--- fullstack-front/src/pages/Home.js
+++ fullstack-front/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import axois from "axois";
-import { async } from 'q';
+import axios from "axios";
 
 export default function Home() {
 
@@ -11,8 +10,8 @@ export default function Home() {
     },[]);
 
     const loadUsers= async () => {
-        const result = await axois.get("http://localhost:8080/users");
-        console.log(result);
+        const result = await axios.get("http://localhost:8080/users");
+        setUsers(result.data);
     }
 
   return (
@@ -22,29 +21,23 @@ export default function Home() {
             <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">User</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
+                {
+                    users.map((user,index)=>(
+                        <tr>
+                            <th scope="row" key={index}>{index+1}</th>
+                            <td>{user.name}</td>
+                            <td>{user.username}</td>
+                            <td>{user.email}</td>
+                        </tr>
+                    ))
+                }
             </tbody>
     </table>
         </div>
