6a2afb793a52cb8e61376c9168581c33bbedc8a0
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-28 07:51:34 +0200

    Change name and add button user

--- fullstack-front/src/layout/Navbar.js
+++ fullstack-front/src/layout/Navbar.js
@@ -4,10 +4,10 @@ export default function
 () {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">
-                    Navbar
+                    Full Stack Application
                 </a>
                 <button
                     className="navbar-toggler"
@@ -20,6 +20,8 @@ export default function
                     >
                         <span className="navbar-toggler-icon"></span>
                     </button>
+
+                    <button className="btn btn-outline-light">Add User</button>
             </div>
         </nav>
     </div>
