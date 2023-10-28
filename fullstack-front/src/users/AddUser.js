f9392d2ed751b652b640d67085d27fd1cb1077b7
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-28 18:19:39 +0200

    Add user inteface

--- fullstack-front/src/users/AddUser.js
+++ fullstack-front/src/users/AddUser.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from 'react'
+import { Link, useNavigate } from "react-router-dom";
 
 export default function AddUser() {
+
+    let navigate=useNavigate()
+
+    const [user, setUser]=useState({
+      name:"",
+      username:"",
+      email:""
+    });
+
+    const{name,username,email}=user;
+
+    const onInputChange=(e)=>{
+        setUser({...user, [e.target.name]: e.target.value});
+    };
+
+    const onSubmit =async (e)=>{
+      e.preventDefault();
+      await axios.post("http://localhost:8080/user", user)
+      navigate("/");
+    };
+  
   return (
     <div className="cointainer">
       <div className='row'>
         <div className='col-md-6 offset-md-3 border rounded p-4 mt-2'>
           <h2 className='text-center m-4'>Register User</h2>
+          <form onSubmit={(e) => onSubmit(e)}>
           <div className='mb-3'>
             <label htmlFor="Name" className="form-label">
               Name
@@ -15,6 +39,8 @@ export default function AddUser() {
               className="form-control"
               placeholder="Enter your name"
               name="name"
+              value={name}
+              onChange={(e)=>onInputChange(e)}
               />
                <div className='mb-3'>
             <label htmlFor="UserName" className="form-label">
@@ -25,6 +51,8 @@ export default function AddUser() {
               className="form-control"
               placeholder="Enter your username"
               name="username"
+              value={username}
+              onChange={(e)=>onInputChange(e)}
               />
                <div className='mb-3'>
             <label htmlFor="Email" className="form-label">
@@ -35,16 +63,19 @@ export default function AddUser() {
               className="form-control"
               placeholder="Enter your email address"
               name="email"
+              value={email}
+              onChange={(e)=>onInputChange(e)}
               />
           <button type="submit" className="btn btn-outline-primary">
             Submit
           </button>
-          <button type="submit" className="btn btn-outline-danger mx-2">
+          <Link className="btn btn-outline-danger mx-2" to="/">
             Cancel
-          </button>
+          </Link>
               </div>
             </div>
           </div>
+          </form>
         </div>
       </div>
     </div>
