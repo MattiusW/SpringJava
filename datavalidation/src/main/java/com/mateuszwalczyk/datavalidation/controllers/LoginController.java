46707d4e3a60ad313102cca03ea63bd13ad095bc
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-04 04:31:51 +0100

    roblem with validation solve need a old version depedency

---
+++ datavalidation/src/main/java/com/mateuszwalczyk/datavalidation/controllers/LoginController.java
@@ -0,0 +1,35 @@
+package com.mateuszwalczyk.datavalidation.controllers;
+
+import com.mateuszwalczyk.datavalidation.models.LoginModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping("/")
+    public String displayLoginForm(Model model){
+        model.addAttribute("loginModel", new LoginModel());
+        return "loginForm.html";
+    }
+
+
+    @PostMapping("/processLogin")
+    public String processLogin(@Valid LoginModel loginModel, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()){
+            model.addAttribute("loginModel", loginModel);
+            return "loginForm.html";
+        }
+
+        model.addAttribute("loginModel", loginModel);
+        return "loginResults.html";
+    }
+}
--- datavalidation/src/main/java/com/mateuszwalczyk/datavalidation/models/LoginModel.java
+++ datavalidation/src/main/java/com/mateuszwalczyk/datavalidation/models/LoginModel.java
@@ -1,8 +1,17 @@
 package com.mateuszwalczyk.datavalidation.models;
 
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
 public class LoginModel {
 
+    @NotNull(message = "Username is required")
+    @Size(min = 3, max = 15, message = "Username must be between 3 and 15")
     private String username;
+    @NotNull
+    @Size(min = 5, max = 15, message = "Password must be between 5 and 15")
     private String password;
 
     public LoginModel(){
