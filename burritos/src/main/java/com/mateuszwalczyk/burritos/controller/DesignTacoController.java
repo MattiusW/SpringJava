30c5665bf3cd827728409fa57f8d3cefaed226d2
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-24 09:38:35 +0100

    Make order class

--- burritos/src/main/java/com/mateuszwalczyk/burritos/controller/DesignTacoController.java
+++ burritos/src/main/java/com/mateuszwalczyk/burritos/controller/DesignTacoController.java
@@ -8,6 +8,7 @@ import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -39,5 +40,6 @@ public class DesignTacoController {
         model.addAttribute("design", new Taco());
         return "design";
     }
+
 
 }
